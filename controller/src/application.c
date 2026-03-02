300a04166621eb637490538d08306efcabcc62f8
Risa123 <risamouris@gmail.com>
2026-03-02 13:06:05 +0100

    controller

---
+++ controller/src/application.c
@@ -0,0 +1,13 @@
+#include <application.h>
+
+
+// Application initialization function which is called once after boot
+void application_init(void){
+    
+}
+
+// Application task function (optional) which is called peridically if scheduled
+void application_task(void){
+    
+    
+}
